e28632e670d0867e0e2e9eb5e3a59d158cefc33a
Karthikan Jeyabalan <35607975+kj666@users.noreply.github.com>
2019-01-28 16:53:24 -0500

    Remove char variable in Quicksort constructor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
 class QuickSort extends Thread{
 
     private ArrayList<Integer> arr;
     private int low;
     private int high;
-    private char side;
-
 
     //constructor
-    public QuickSort(ArrayList<Integer> arr, int low, int high, char side){
+    public QuickSort(ArrayList<Integer> arr, int low, int high){
         this.arr = arr;
         this.low = low;
         this.high = high;
-        this.side = side;
     }
 
 
@@ -33,11 +29,10 @@ class QuickSort extends Thread{
             int pivot = partition(arr, low, high);       //partition sort between low and high
             quickSort(arr, low, pivot - 1);
             quickSort(arr, pivot + 1, high);
-        QuickSort left = new QuickSort(arr,low, pivot-1, 'L');  //QuickSort thread for the left array
-         left.start();
-
-      QuickSort right = new QuickSort(arr, pivot + 1, high, 'R'); //QuickSort thread for the right array
-          right.start();
+            QuickSort left = new QuickSort(arr,low, pivot-1);  //QuickSort thread for the left array
+            QuickSort right = new QuickSort(arr, pivot + 1, high); //QuickSort thread for the right array
+            left.start();
+            right.start();
 
          try {
              left.join();
@@ -74,6 +69,7 @@ class QuickSort extends Thread{
     }
 
     public void print(){
+
         System.out.println(arr);
     }
 }
@@ -85,7 +81,7 @@ public class Main {
         ArrayList<Integer> number = parse();
         System.out.println(number);
         System.out.println("The "+Thread.currentThread().getName()+" thread just finished filling an integer array");
-        QuickSort sort = new QuickSort(number, 0, number.size()-1, 'N');
+        QuickSort sort = new QuickSort(number, 0, number.size()-1);
         sort.quickSort(number, 0, number.size()-1);
         printOutput(number);
 
