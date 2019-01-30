07a10a31e5162a8113d8b3e7aefcc4c40ff92e5f
Karthikan Jeyabalan <35607975+kj666@users.noreply.github.com>
2019-01-30 16:12:42 -0500

    changed how thread starts in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 class QuickSort extends Thread{
 
+    //class members
     private ArrayList<Integer> arr;
     private int low;
     private int high;
@@ -17,23 +17,24 @@ class QuickSort extends Thread{
         this.high = high;
     }
 
-
+    //thread implementation
     public void run(){
        quickSort(arr, low, high);
-        System.out.println(arr);
-
     }
 
+    //quicksort algorithm
     public static void quickSort(ArrayList<Integer> arr, int low, int high){
         if (high > low) {
             int pivot = partition(arr, low, high);       //partition sort between low and high
-            quickSort(arr, low, pivot - 1);
-            quickSort(arr, pivot + 1, high);
+
+            //create threads
             QuickSort left = new QuickSort(arr,low, pivot-1);  //QuickSort thread for the left array
             QuickSort right = new QuickSort(arr, pivot + 1, high); //QuickSort thread for the right array
+            //run the threads
             left.start();
             right.start();
 
+            //make the threads concurrent
          try {
              left.join();
              right.join();
@@ -42,19 +43,17 @@ class QuickSort extends Thread{
              System.out.println("An EXCEPTION OCCURED !!!");
          }
         }
-
     }
 
+    //compare array method
     public static int partition(ArrayList<Integer> arr, int low, int high) {
 
-        int pivot=arr.get(high);
-        System.out.print("Pivot: " + pivot+ " ");
-        int lo = low;                   //assign low to temp
+        int pivot=arr.get(high);        //choose highest element as pivot
+        int lo = low;                   //assign low to temporary variable
 
         for ( int i = lo; i < high; i++) {
             if (arr.get(i) <= pivot) {    //compare array with pivot and if smaller than pivot exchange
                 swap(arr, lo, i);
-                System.out.println(arr);
                 lo++;                   //move the boundary between items compared with pivot
             }
         }
@@ -67,22 +66,19 @@ class QuickSort extends Thread{
         arr.set(a, arr.get(b));
         arr.set(b, temp);
     }
-
-    public void print(){
-
-        System.out.println(arr);
-    }
 }
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException, InterruptedException {
         ArrayList<Integer> number = parse();
         System.out.println(number);
         System.out.println("The "+Thread.currentThread().getName()+" thread just finished filling an integer array");
+        //create the quickSort Thread
         QuickSort sort = new QuickSort(number, 0, number.size()-1);
-        sort.quickSort(number, 0, number.size()-1);
+        sort.start();
+        sort.join();
+
         printOutput(number);
 
     }
@@ -99,13 +95,13 @@ public class Main {
         return number;
     }
 
+    //output the array to file
     public static void printOutput(ArrayList<Integer> number) throws IOException{
+        System.out.println(number);
         PrintWriter writer = new PrintWriter("output.txt");
         for(int i = 0; i< number.size(); i++){
             writer.println(number.get(i));
         }
         writer.close();
     }
-
 }
-
